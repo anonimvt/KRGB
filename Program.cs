7cfa6195f47dc3658748ba4bdbccf84484084357
anonimvt <rinat.madyarov.08@gmail.com>
2023-03-11 14:18:38 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string[] arr1 = new string[4] {"string", "23", "hi", "request"};
+string[] arr2 = new string[arr1.Length];
+
+
+void SecondArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(arr1, arr2);
+PrintArray(arr2);
